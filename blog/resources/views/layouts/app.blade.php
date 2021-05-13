3cd9ef0aa0dc8f34cc78563445cc955b65fb4382
jimtronchet <79514914+jimtronchet@users.noreply.github.com>
2021-05-13 12:31:23 +0200

    Dark mode fixes en een nieuwe switch toegevoegd.
    + Switch voor darkmode
    + Correcte styling/padding
    + Linkjes werken en uitloggen functionaliteit is nu gefixt

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -9,38 +9,7 @@
 
 
         <title>Persoonlijk Ontwikkelings Plan</title>
-<style>
-    [class^="hex-icon"] {
-        width: 42px;
-        height: 50px;
-        margin: 0 10px;
-        display: inline-block;
-        -webkit-transition: all 0.2s cubic-bezier(0.215, 0.610, 0.355, 1.000);
-        transition: all 0.2s cubic-bezier(0.215, 0.610, 0.355, 1.000);
-    }
 
-    [class^="hex-icon"] svg {
-        width: 100%;
-        height: 100%;
-        display: block;
-    }
-
-    .hex-icon-sun path:first-of-type {
-        fill: #111827;
-    }
-
-    .hex-icon-sun circle {
-        stroke: #fff;
-        stroke-width: 2px;
-        fill: none;
-    }
-
-    .hex-icon-sun circle:last-of-type {
-        stroke-width: 2px;
-        stroke-dasharray: 2,7.4;
-    }
-
-</style>
     </head>
 
     <body class="bg-white">
@@ -66,42 +35,52 @@
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-4 flex items-center md:ml-6">
-                            <button id="switchTheme" class="hex-icon-sun p-0 w-10 h-10 rounded-full active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
-                                <svg>
-                                    <path d="M19,1 Q21,0,23,1 L39,10 Q41.5,11,42,14 L42,36 Q41.5,39,39,40 L23,49 Q21,50,19,49 L3,40 Q0.5,39,0,36 L0,14 Q0.5,11,3,10 L19,1"></path>
-                                    <circle cx="21" cy="25" r="8"></circle>
-                                    <circle cx="21" cy="25" r="12">
-                                        <animateTransform attributeName="transform" attributeType="XML" type="rotate" from="0 21 25" to="360 21 25" dur="3.5s" repeatCount="indefinite"></animateTransform>
-                                    </circle>
-                                </svg>
-                            </button>
+                            <label class="switch">
+                                <input id="switchTheme" type="checkbox">
+                                <span class="slider round bg-gray-600"></span>
+                            </label>
+                            @auth
+                                <div class="ml-3 relative">
+                                    <div class="ml-10">
+                                        <button onclick="showDropdown()" class="max-w-xs bg-gray-700 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-900 focus:ring-white" id="user-menu" aria-haspopup="true">
+                                            <span class="sr-only">Open gebruiker menu</span>
+                                            <img class="h-8 w-8 rounded-full" src="https://e-cdn-images.dzcdn.net/images/artist/ef0de2a0948a621339447019db3e52b0/264x264-000000-80-0-0.jpg" alt="">
+                                        </button>
+                                    </div>
+
+
+                                    <div id="site-overlay" style="display:none; z-index: 1;" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+
+                                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Instellingen</a>
+
+                                        <form action="{{ route('logout') }}" method="post" class="inline">
+                                            @csrf
+                                            <button type="submit" class="text-left block w-full mr-0 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" role="menuitem">
+                                                Uitloggen
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>
+
+                                @endauth
                             @guest
                                 <div class="ml-10 flex items-baseline space-x-4">
-                                    <a href="{{ route('login')}}" style="transition: all .15s ease" class="dark:text-gray-300 dark:hover:bg-gray-600 hover:bg-gray-200 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium">Inloggen</a>
+                                    <a href="{{ route('login')}}" style="transition: background-color .15s ease" class="dark:text-gray-300 dark:hover:bg-gray-600 hover:bg-gray-200 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium">Inloggen</a>
 
-                                    <a href="{{ route('register')}}" style="transition: all .15s ease" class="dark:text-gray-300 dark:hover:bg-gray-600 hover:bg-blue-700 dark:hover:text-white dark:bg-gray-700 bg-blue-600 text-white px-3 py-2 rounded-md text-sm font-medium">Registreren</a>
+                                    <a href="{{ route('register')}}" style="transition: background-color .15s ease" class="dark:text-gray-300 dark:hover:bg-gray-600 hover:bg-blue-700 dark:hover:text-white dark:bg-gray-700 bg-blue-600 text-white px-3 py-2 rounded-md text-sm font-medium">Registreren</a>
                                 </div>
                             @endguest
-                                @auth
-                                    <div class="ml-10 flex items-baseline space-x-4">
-                                        <a href="{{ route('logout')}}" style="transition: all .15s ease" class="dark:text-gray-300 dark:hover:bg-gray-600 hover:bg-gray-200 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium">Uitloggen</a>
-                                    </div>
-                                @endauth
 
                         </div>
                     </div>
-                    <div class="-mr-2 flex md:hidden">
+                    <div class="-mr-2 flex md:hidden" style="align-items: center;">
                         <!-- Mobile menu button -->
-                        <button id="switchTheme1" class="hex-icon-sun p-0 w-10 h-10 rounded-full active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
-                            <svg>
-                                <path d="M19,1 Q21,0,23,1 L39,10 Q41.5,11,42,14 L42,36 Q41.5,39,39,40 L23,49 Q21,50,19,49 L3,40 Q0.5,39,0,36 L0,14 Q0.5,11,3,10 L19,1"></path>
-                                <circle cx="21" cy="25" r="8"></circle>
-                                <circle cx="21" cy="25" r="12">
-                                    <animateTransform attributeName="transform" attributeType="XML" type="rotate" from="0 21 25" to="360 21 25" dur="3.5s" repeatCount="indefinite"></animateTransform>
-                                </circle>
-                            </svg>
-                        </button>
-                        <button type="button" onclick="mobileDropdown()" class="dark:bg-gray-600 hover:bg-gray-200 inline-flex items-center justify-center p-2 rounded-md dark:text-gray-400 dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                        <label class="switch">
+                            <input id="switchTheme1" type="checkbox">
+                            <span class="slider round bg-gray-600"></span>
+                        </label>
+
+                        <button type="button" onclick="mobileDropdown()" class="ml-10 dark:bg-gray-600 hover:bg-gray-200 inline-flex items-center justify-center p-2 rounded-md dark:text-gray-400 dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-gray-800 focus:ring-gray-300" aria-controls="mobile-menu" aria-expanded="false">
                             <span class="sr-only">Open hoofdmenu</span>
 
                             <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -125,34 +104,30 @@
 
                     <a href="#" class="dark:text-gray-300 dark:hover:bg-gray-600 hover:bg-gray-200 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
 
-                    <div class="pt-4 pb-3 border-t dark:border-gray-500">
+                    <div class="border-t dark:border-gray-500">
                         @guest
-                            <div class=" pt-2 pb-3 space-y-1">
-                                <a href="{{ route('login')}}" style="transition: all .15s ease" class="dark:text-gray-300 dark:hover:bg-gray-600 hover:bg-gray-200 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium">Inloggen</a>
+                                <a href="{{ route('login')}}" style="transition: all .15s ease; margin-top:4px;" class="dark:text-gray-300 dark:hover:bg-gray-600 hover:bg-gray-200 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium">Inloggen</a>
 
-                                <a href="{{ route('register')}}" style="transition: all .15s ease" class="dark:text-gray-300 dark:hover:bg-gray-600 hover:bg-gray-200 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium">Registreren</a>
-                            </div>
+                                <a href="{{ route('register')}}" style="transition: all .15s ease; margin-top:4px;" class="dark:text-gray-300 dark:hover:bg-gray-600 hover:bg-gray-200 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium">Registreren</a>
                         @endguest
                         @auth
-                            <div class="flex items-center px-5">
+                            <div style="margin-top:4px; " class="flex items-center px-3 py-2 ">
                                 <div class="flex-shrink-0">
                                     <img class="h-10 w-10 rounded-full" src="https://e-cdn-images.dzcdn.net/images/artist/ef0de2a0948a621339447019db3e52b0/264x264-000000-80-0-0.jpg" alt="">
                                 </div>
                                 <div class="ml-3">
                                     <div class="text-base font-medium leading-none dark:text-white">Voornaam Achternaam</div>
-                                    <div class="text-sm font-medium leading-none dark:text-gray-400">e-mail</div>
+                                    <div class="text-sm mt-1 font-medium leading-none dark:text-gray-400">e-mail</div>
                                 </div>
                             </div>
-                            <div class="mt-3 px-2 space-y-1">
                                 <a href="#" class="block px-3 py-2 rounded-md text-base font-medium dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 hover:bg-gray-200 ">Instellingen</a>
 
-                                <form action="#" method="post" class="">
+                                <form action="{{ route('logout') }}" method="get" class="">
                                     @csrf
                                     <button type="submit" class="w-full text-left block px-3 py-2 rounded-md text-base font-medium dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 hover:bg-gray-200 " role="menuitem">
                                         Uitloggen
                                     </button>
                                 </form>
-                            </div>
                         @endauth
                     </div>
                 </div>
@@ -166,6 +141,7 @@
     </body>
 
     <script>
+
         if (localStorage.theme === 'dark' || (!'theme' in localStorage && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
             document.querySelector('html').classList.add('dark')
         } else if (localStorage.theme === 'dark') {
@@ -193,6 +169,17 @@
                 localStorage.theme = 'dark';
             }
         });
+
+
+        function showDropdown() {
+            var x = document.getElementById("site-overlay");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+                x.style.display = "none";
+            }
+        }
 
         function mobileDropdown() {
             var x = document.getElementById("menu-mobile");
