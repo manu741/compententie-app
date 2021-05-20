877c9c798de26d19be4bab9b51ba09acf1e7cea6
Damien Lucien van der Meulen <71441227+Gliese671@users.noreply.github.com>
2021-05-20 14:46:57 +0200

    Login+Register scherm is nu mooi
    Erg mooi.

--- blog/resources/views/auth/login.blade.php
+++ blog/resources/views/auth/login.blade.php
@@ -2,19 +2,40 @@
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-4/12 bg-white p-6 rounded-lg">
+        <div class="w-full bg-white dark:bg-gray-600 p-6">
             @if (session('status'))
                 <div class="bg-red-500 p-4 rounded-lg mb-6 text-white text-center">
                     {{ session('status') }}
                 </div>
             @endif
 
+                <div class="flex justify-center">
+                    <div class="w-8/12 flex justify-center rounded-lg">
+                        <div class="relative overflow-hidden">
+                            <div class="relative z-10 sm:pb-16 md:pb-20 lg:w-full lg:pb-28 xl:pb-16">
+                                <main class="justify-center mt-10 mx-auto px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-14">
+                                    <div class="sm:text-center lg:text-center">
+                                        <h1 class="text-4xl tracking-tight font-bold text-gray-900 sm:text-4xl md:text-4xl mb-8">
+                                            <span class="lg:text-center block text-black dark:text-white xl:inline mb-10">Inloggen</span>
+                                        </h1>
+                                        <span class="mt-3 text-base text-gray-500 dark:text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                            Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad.
+                                        </span>
+                                    </div>
+                                </main>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             <form action="{{ route('login') }}" method="post">
                 @csrf
 
-                <div class="mb-4">
+                <p class="text-black dark:text-white text-center font-bold mb-4">E-mail</p>
+
+                <div class="mb-4 text-center">
                     <label for="email" class="sr-only">Email</label>
-                    <input type="text" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+                    <input type="text" name="email" id="email" placeholder="" class="bg-gray-100 dark:bg-gray-500 dark:border-gray-400 border-2 w-3/12 p-4 focus:outline-none @error('email') border-red-500 @enderror" value="{{ old('email') }}">
 
                     @error('email')
                         <div class="text-red-500 mt-2 text-sm">
@@ -23,9 +44,11 @@
                     @enderror
                 </div>
 
-                <div class="mb-4">
+                <p class="text-black dark:text-white text-center font-bold mb-4">Wachtwoord</p>
+
+                <div class="mb-4 text-center">
                     <label for="password" class="sr-only">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Choose a password" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror" value="">
+                    <input type="password" name="password" id="password" placeholder="" class="bg-gray-100 dark:bg-gray-500 dark:border-gray-400 border-2 w-3/12 p-4 focus:outline-none @error('password') border-red-500 @enderror" value="">
 
                     @error('password')
                         <div class="text-red-500 mt-2 text-sm">
@@ -34,15 +57,15 @@
                     @enderror
                 </div>
 
-                <div class="mb-4">
-                    <div class="flex items-center">
+                <div class="mb-4 flex justify-center">
+                    <div class="dark:text-white">
                         <input type="checkbox" name="remember" id="remember" class="mr-2">
                         <label for="remember">Remember me</label>
                     </div>
                 </div>
 
-                <div>
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Login</button>
+                <div class="flex justify-center">
+                    <button type="submit" class="bg-blue-500 text-white dark:bg-green-400 px-4 py-3 mb-10 rounded font-medium w-2/12">Login</button>
                 </div>
             </form>
         </div>
--- blog/resources/views/auth/register.blade.php
+++ blog/resources/views/auth/register.blade.php
@@ -2,12 +2,32 @@
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-4/12 bg-white p-6 rounded-lg">
+        <div class="w-full bg-white dark:bg-gray-600 p-6">
+            <div class="flex justify-center">
+                <div class="w-8/12 flex justify-center rounded-lg">
+                    <div class="relative overflow-hidden">
+                        <div class="relative z-10 sm:pb-16 md:pb-20 lg:w-full lg:pb-28 xl:pb-16">
+                            <main class="justify-center mt-10 mx-auto px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-14">
+                                <div class="sm:text-center lg:text-center">
+                                    <h1 class="text-4xl tracking-tight font-bold text-gray-900 sm:text-4xl md:text-4xl mb-8">
+                                        <span class="lg:text-center block text-black dark:text-white xl:inline mb-10">Registreren</span>
+                                    </h1>
+                                    <span class="mt-3 text-base text-gray-500 dark:text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                            Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad.
+                                    </span>
+                                </div>
+                            </main>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <form action="{{ route('register') }}" method="post">
                 @csrf
-                <div class="mb-4">
+
+                <p class="text-black dark:text-white text-center font-bold mb-4">Naam:</p>
+                <div class="mb-4 w-full text-center">
                     <label for="firstname" class="sr-only">Name</label>
-                    <input type="text" name="firstname" id="firstname" placeholder="First name" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('firstname') border-red-500 @enderror" value="{{ old('firstname') }}">
+                    <input type="text" name="firstname" id="firstname" placeholder="Naam" class="dark:bg-gray-500  bg-gray-100 dark:border-gray-400 shadow w-4/12 border-2 p-4 focus:outline-none @error('firstname') border-red-500 @enderror" value="{{ old('firstname') }}">
 
                     @error('firstname')
                         <div class="text-red-500 mt-2 text-sm">
@@ -16,9 +36,10 @@
                     @enderror
                 </div>
 
-                <div class="mb-4">
+                <p class="text-black dark:text-white text-center font-bold mb-4">Achternaam:</p>
+                <div class="mb-4 w-full text-center">
                     <label for="lastname" class="sr-only">Name</label>
-                    <input type="text" name="lastname" id="lastname" placeholder="Last name" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('lastname') border-red-500 @enderror" value="{{ old('lastname') }}">
+                    <input type="text" name="lastname" id="lastname" placeholder="Achternaam" class="dark:bg-gray-500 bg-gray-100 dark:border-gray-400 shadow w-4/12 border-2 p-4 focus:outline-none @error('lastname') border-red-500 @enderror" value="{{ old('lastname') }}">
 
                     @error('lastname')
                     <div class="text-red-500 mt-2 text-sm">
@@ -27,9 +48,10 @@
                     @enderror
                 </div>
 
-                <div class="mb-4">
+                <p class="text-black dark:text-white text-center font-bold mb-4">Studenten Nummer:</p>
+                <div class="mb-4 w-full text-center">
                     <label for="studentnr" class="sr-only">Studentnr</label>
-                    <input type="text" name="studentnr" id="studentnr" placeholder="Student number" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('studentnr') border-red-500 @enderror" value="{{ old('studentnr') }}">
+                    <input type="text" name="studentnr" id="studentnr" placeholder="1234567" class="dark:bg-gray-500 bg-gray-100 dark:border-gray-400 shadow w-4/12 border-2 p-4 focus:outline-none @error('studentnr') border-red-500 @enderror" value="{{ old('studentnr') }}">
 
                     @error('studentnr')
                         <div class="text-red-500 mt-2 text-sm">
@@ -38,9 +60,10 @@
                     @enderror
                 </div>
 
-                <div class="mb-4">
+                <p class="text-black dark:text-white text-center font-bold mb-4">School E-mail:</p>
+                <div class="mb-4 w-full text-center">
                     <label for="email" class="sr-only">Email</label>
-                    <input type="text" name="email" id="email" placeholder="Your schoolmail" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+                    <input type="text" name="email" id="email" placeholder="example.example@student.hu.nl" class="dark:bg-gray-500 bg-gray-100 dark:text-white dark:border-gray-400 shadow w-4/12 border-2 p-4 focus:outline-none @error('email') border-red-500 @enderror" value="{{ old('email') }}">
 
                     @error('email')
                         <div class="text-red-500 mt-2 text-sm">
@@ -49,9 +72,10 @@
                     @enderror
                 </div>
 
-                <div class="mb-4">
+                <p class="text-black dark:text-white text-center font-bold mb-4">Wachtwoord</p>
+                <div class="mb-4 w-full text-center">
                     <label for="password" class="sr-only">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Choose a password" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror" value="">
+                    <input type="password" name="password" id="password" placeholder="" class="dark:bg-gray-500 bg-gray-100 dark:border-gray-400 shadow w-4/12 border-2 p-4 focus:outline-none @error('password') border-red-500 @enderror" value="">
 
                     @error('password')
                         <div class="text-red-500 mt-2 text-sm">
@@ -60,9 +84,10 @@
                     @enderror
                 </div>
 
-                <div class="mb-4">
+                <p class="text-black dark:text-white text-center font-bold mb-4">Wachtwoord Herhalen</p>
+                <div class="mb-4 w-full flex justify-center">
                     <label for="password_confirmation" class="sr-only">Password again</label>
-                    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Repeat your password" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password_confirmation') border-red-500 @enderror" value="">
+                    <input type="password" name="password_confirmation" id="" placeholder="" class="dark:bg-gray-500 bg-gray-100 dark:border-gray-400 shadow w-4/12 border-2 w-4/12 p-4 mb-4 focus:outline-none @error('password_confirmation') border-red-500 @enderror" value="">
 
                     @error('password_confirmation')
                         <div class="text-red-500 mt-2 text-sm">
@@ -71,10 +96,11 @@
                     @enderror
                 </div>
 
-                <div>
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Register</button>
+                <div class="flex justify-center">
+                    <button type="submit" class="w-2/12 dark:bg-green-400 bg-blue-500 text-white px-4 py-3 rounded font-medium">Register</button>
                 </div>
             </form>
         </div>
+
     </div>
 @endsection
