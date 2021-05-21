a4f4eee3f038a2a9965684bff5881b92168817de
jimtronchet <79514914+jimtronchet@users.noreply.github.com>
2021-05-21 12:13:41 +0200

    Fix met Darktheme/lighttheme

--- blog/resources/views/users/afspraken.blade.php
+++ blog/resources/views/users/afspraken.blade.php
@@ -24,29 +24,31 @@
         </div>
 
         <div class="flex justify-center">
-            <div class=" dark:bg-gray-600 w-8/12 px-8">
-
-                <div class="w-full my-2">
-                    <label class=" my-2">Pop-cyclus:
-                        <div class="my-2 relative inline-flex">
-                            <svg class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
-                            <select class="border border-gray-300 text-gray-600 bg-white hover:border-gray-400 focus:outline-none appearance-none px-3 py-2 dark:bg-gray-500 dark:text-gray-400 dark:border-gray-600 dark:hover:border-gray-700 hover:border-gray-400" style="width:180px;">
-                                <option>1e Cyclus 20-21</option>
-                                <option>2e Cyclus 20-21</option>
-                                <option>1e Cyclus 21-22</option>
-                                <option>2e Cyclus 21-22</option>
-                            </select>
-                        </div>
-                    </label>
-
-                    <div class="my-2 relative inline-flex">
+            <div class="w-8/12 flex">
+                <div class="mt-5 mr-auto mb-5">
+                    <span class="mr-4">Pop-cyclus:</span>
+                    <div class="relative inline-flex">
                         <svg class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
-                        <select class="border border-gray-300 text-gray-600 bg-white hover:border-gray-400 focus:outline-none appearance-none px-3 py-2 dark:bg-gray-500 dark:text-gray-400 dark:border-gray-600 dark:hover:border-gray-700 hover:border-gray-400" style="width:250px;">
+                        <select class="border border-gray-300 text-gray-600 bg-white hover:border-gray-400 focus:outline-none appearance-none px-3 py-2 dark:bg-gray-500 dark:text-gray-400 dark:border-gray-600 dark:hover:border-gray-700 hover:border-gray-400" style="width:180px;">
+                            <option>1e Cyclus 20-21</option>
+                            <option>2e Cyclus 20-21</option>
+                            <option>1e Cyclus 21-22</option>
+                            <option>2e Cyclus 21-22</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="mt-2 mb-2 ml-auto">
+                    <div class="relative inline-flex">
+
+                        <svg class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
+                        <select class="border border-gray-300 text-gray-600 bg-white hover:border-gray-400 focus:outline-none appearance-none px-3 py-2 dark:bg-gray-500 dark:text-gray-400 dark:border-gray-600 dark:hover:border-gray-700 hover:border-gray-400" style="width:180px;">
                             <option>Opleiding</option>
                         </select>
                     </div>
                 </div>
-        </div>
+
+            </div>
         </div>
 
 
