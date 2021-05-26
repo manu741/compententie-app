ba66c30bed9ccfc8b83423eca4cedc5da9b7f769
manu741 <emanouil.salamanis@student.hu.nl>
2021-05-26 13:55:01 +0200

    auth

--- blog/resources/views/settings.blade.php
+++ blog/resources/views/settings.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 
-    <div class="flex justify-center">
+
+
+        <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
 
             <div style='border-bottom: 2px solid #eaeaea'>
@@ -13,84 +15,132 @@
                 </ul>
             </div>
 
-            <div class="mt-5 md:mt-0 md:col-span-2 active" id="personal_data" data-tab-content>
-                <form action="{{ route('settings') }}" method="POST">
-                    @csrf
-                    <div class="shadow overflow-hidden sm:rounded-md">
-                        <div class="px-4 py-5 bg-white sm:p-6">
-                            <div class="grid grid-cols-5 gap-5">
+            @if (auth()->user()->clearance == 'admin')
 
-                                <div class="col-span-5 sm:col-span-3 lg:col-span-2">
-                                    <label for="firstname" class="block text-sm font-medium text-gray-700">Voornaam</label>
-                                    <input value="{{ auth()->user()->firstname }}" type="text"  placeholder="Voornaam" name="firstname" id="firstname" autocomplete="given-name" class=" p-1.5 px-3 border-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md @error('firstname') border-red-500 @enderror">
+                <div class="mt-5 md:mt-0 md:col-span-2 active" id="personal_data" data-tab-content>
+                    <form action="{{ route('settings') }}" method="POST">
+                        @csrf
+                        <div class="shadow overflow-hidden sm:rounded-md">
+                            <div class="px-4 py-5 bg-white sm:p-6">
+                                <div class="grid grid-cols-5 gap-5">
 
-                                    @error('firstname')
-                                    <div class="text-red-500 mt-2 text-sm">
-                                        {{ $message }}
+                                    <div class="col-span-5 sm:col-span-3 lg:col-span-2">
+                                        <label for="firstname" class="block text-sm font-medium text-gray-700">Voornaam</label>
+                                        <input value="{{ auth()->user()->firstname }}" type="text"  placeholder="Voornaam" name="firstname" id="firstname" autocomplete="given-name" class=" p-1.5 px-3 border-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md @error('firstname') border-red-500 @enderror">
+
+                                        @error('firstname')
+                                        <div class="text-red-500 mt-2 text-sm">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
                                     </div>
-                                    @enderror
-                                </div>
 
-                                <div class="col-span-5 sm:col-span-3 lg:col-span-2">
-                                    <label for="lastname" class="block text-sm font-medium text-gray-700">Achternaam</label>
-                                    <input value="{{ auth()->user()->lastname }}" type="text" name="lastname" placeholder="Achternaam"  id="lastname" class="  p-1.5 px-3 border-2  mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md @error('lastname') border-red-500 @enderror">
-                                    @error('lastname')
-                                    <div class="text-red-500 mt-2 text-sm">
-                                        {{ $message }}
+                                    <div class="col-span-5 sm:col-span-3 lg:col-span-2">
+                                        <label for="lastname" class="block text-sm font-medium text-gray-700">Achternaam</label>
+                                        <input value="{{ auth()->user()->lastname }}" type="text" name="lastname" placeholder="Achternaam"  id="lastname" class="  p-1.5 px-3 border-2  mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md @error('lastname') border-red-500 @enderror">
+                                        @error('lastname')
+                                        <div class="text-red-500 mt-2 text-sm">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
                                     </div>
-                                    @enderror
-                                </div>
 
-                                <div class="col-span-5 sm:col-span-3 lg:col-span-2">
-                                    <label for="studentnr" class="block text-sm font-medium text-gray-700">studentnr</label>
-                                    <input value="{{ auth()->user()->studentnr  }}" type="text" name="studentnr" placeholder="studentnr"  id="studentnr"  class=" p-1.5 px-3 border-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md @error('studentnr') border-red-500 @enderror">
-                                    @error('studentnr')
-                                    <div class="text-red-500 mt-2 text-sm">
-                                        {{ $message }}
+                                    <div class="col-span-5 sm:col-span-3 lg:col-span-2">
+                                        <label for="studentnr" class="block text-sm font-medium text-gray-700">studentnr</label>
+                                        <input value="{{ auth()->user()->studentnr  }}" type="text" name="studentnr" placeholder="studentnr"  id="studentnr"  class=" p-1.5 px-3 border-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md @error('studentnr') border-red-500 @enderror">
+                                        @error('studentnr')
+                                        <div class="text-red-500 mt-2 text-sm">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
                                     </div>
-                                    @enderror
-                                </div>
 
-                                <div class="col-span-5 sm:col-span-3 lg:col-span-3">
-                                    <label for="email" class="block text-sm font-medium text-gray-700">Email adres</label>
-                                    <input value="{{ auth()->user()->email  }}" type="text" name="email" placeholder="E-mail adres"  id="email"  class=" p-1.5 px-3 border-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md @error('email') border-red-500 @enderror">
-                                    @error('email')
-                                    <div class="text-red-500 mt-2 text-sm">
-                                        {{ $message }}
+                                    <div class="col-span-5 sm:col-span-3 lg:col-span-3">
+                                        <label for="email" class="block text-sm font-medium text-gray-700">Email adres</label>
+                                        <input value="{{ auth()->user()->email  }}" type="text" name="email" placeholder="E-mail adres"  id="email"  class=" p-1.5 px-3 border-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md @error('email') border-red-500 @enderror">
+                                        @error('email')
+                                        <div class="text-red-500 mt-2 text-sm">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
                                     </div>
-                                    @enderror
-                                </div>
 
-                                <div class="col-span-6 sm:col-span-3">
-                                    <label for="password" class="block text-sm font-medium text-gray-700">Wachtwoord aanpassen</label>
-                                    <input type="password" name="password" id="password" class=" p-1.5 px-3 border-2  mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md @error('password') border-red-500 @enderror">
-                                    @error('password')
-                                    <div class="text-red-500 mt-2 text-sm">
-                                        {{ $message }}
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label for="password" class="block text-sm font-medium text-gray-700">Wachtwoord aanpassen</label>
+                                        <input type="password" name="password" id="password" class=" p-1.5 px-3 border-2  mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md @error('password') border-red-500 @enderror">
+                                        @error('password')
+                                        <div class="text-red-500 mt-2 text-sm">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
                                     </div>
-                                    @enderror
-                                </div>
 
-                                <div class="col-span-6 sm:col-span-3">
-                                    <label for="password" class="block text-sm font-medium text-gray-700">Wachtwoord herhalen</label>
-                                    <input type="password" name="password_confirmation" id="password" class=" p-1.5 px-3 border-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md @error('password') border-red-500 @enderror">
-                                    @error('password')
-                                    <div class="text-red-500 mt-2 text-sm">
-                                        {{ $message }}
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label for="password" class="block text-sm font-medium text-gray-700">Wachtwoord herhalen</label>
+                                        <input type="password" name="password_confirmation" id="password" class=" p-1.5 px-3 border-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md @error('password') border-red-500 @enderror">
+                                        @error('password')
+                                        <div class="text-red-500 mt-2 text-sm">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
                                     </div>
-                                    @enderror
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="px-4 py-3 bg-gray-50 text-left sm:px-6">
-                            <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                Gegevens aanppassen
-                            </button>
+                            <div class="px-4 py-3 bg-gray-50 text-left sm:px-6">
+                                <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    Gegevens aanppassen
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                </form>
-            </div>
+                    </form>
+                </div>
+
+            @else
+
+
+                <div class="mt-5 md:mt-0 md:col-span-2 active" id="personal_data" data-tab-content>
+                            <div class="px-4 py-5 bg-white sm:p-6">
+                                <div class="grid grid-cols-5 gap-5">
+
+                                    <div class="col-span-5 sm:col-span-3 lg:col-span-3">
+                                        <label for="email" class="block text-sm font-medium text-gray-700">Voornaam</label>
+                                        <div>
+                                            {{ auth()->user()->firstname }}
+                                        </div>
+                                    </div>
+
+                                    <div class="col-span-5 sm:col-span-3 lg:col-span-3">
+                                        <label for="email" class="block text-sm font-medium text-gray-700">Achternaam</label>
+                                        <div>
+                                            {{ auth()->user()->lastname }}
+                                        </div>
+                                    </div>
+
+                                    <div class="col-span-5 sm:col-span-3 lg:col-span-3">
+                                        <label for="email" class="block text-sm font-medium text-gray-700">Student nummer</label>
+                                        <div>
+                                            {{ auth()->user()->studentnr }}
+                                        </div>
+                                    </div>
+
+                                    <div class="col-span-5 sm:col-span-3 lg:col-span-3">
+                                        <label for="email" class="block text-sm font-medium text-gray-700">Email adres</label>
+                                        <div>
+                                            {{ auth()->user()->email }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                </div>
+
+
+
+
+
+
+            @endif
 
             <div class="mt-5 md:mt-0 md:col-span-2" id="study" data-tab-content>
                 <form action="" method="GET">
@@ -104,7 +154,7 @@
                                     <select id="your_study" name="your_study" class=" mt-1 block w-full py-2 px-3 border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm p-1.5 px-3 border-2  @error('your_study') border-red-500 @enderror">
                                         <option disabled selected value> -- Kies één van de opties -- </option>
                                         <option value="Ja">Ja</option>
-                                        <option value="Nee">Nee</option>
+                                 <option value="Nee">Nee</option>
                                     </select>
 
                                     @error('your_study')
