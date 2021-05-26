2cbc2a0742eceae418ac63246721ed97f0702a06
Damien Lucien van der Meulen <71441227+Gliese671@users.noreply.github.com>
2021-05-26 11:16:51 +0200

    POP Pagina Gemaakt
    POP Pagina is er nu, woohoo

--- blog/resources/views/components/popmenu.blade.php
+++ blog/resources/views/components/popmenu.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white dark:bg-gray-600 flex justify-between text-center block text-base font-medium pt-4 pb-4 mb-5 w-full">
+<div class="bg-white dark:bg-gray-600 flex justify-between text-center block text-base font-medium pt-4 pb-4 w-full">
     <div class="w-9/12 flex m-auto justify-between">
         <div class="flex mx-2">
             <a href="{{route('nulsituatie')}}" class="dark:text-gray-300 dark:hover:bg-gray-600 hover:bg-gray-200 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium">Nulsituatie</a>
--- blog/resources/views/users/reflectie.blade.php
+++ blog/resources/views/users/reflectie.blade.php
@@ -93,7 +93,7 @@
                         <svg class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
                         <select class="border border-gray-300 text-gray-600 bg-white hover:border-gray-400 focus:outline-none appearance-none px-3 py-2 bg-gray-200 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-500 dark:hover:border-gray-800 hover:border-gray-400">
                             <option>Niveau</option>
-                            <option>Niveau 69</option>
+                            <option>Niveau 70</option>
                         </select>
                     </div>
                 </div>
@@ -203,23 +203,30 @@
 
         <div class="flex justify-center">
             <div class="bg-gray-100 dark:bg-gray-700 w-8/12 rounded mb-4">
-                <span class="ml-12 mt-8">Preview reflectie (klik 'Details' om te wijzigen)
-                    <span class="flex mb-4 ml-12">Situatie
-                    <input type='text' placeholder="Omdat..." class="w-full mt-2 mb-6 px-6 py-3 ml-12 border rounded-lg text-lg max-w-xl text-gray-700 focus:outline-none bg-gray-200 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-500 dark:hover:border-gray-800 hover:border-gray-400"/>
-                    </span>
+                <div class="ml-12 mt-8">Preview reflectie (klik 'Details' om te wijzigen)
+                    <div>
+                        <span class="flex mb-4 ml-12">Situatie:
+                        <input type='text' placeholder="Omdat..." class="w-full mt-2 mb-6 px-6 py-3 ml-12 border rounded-lg text-lg max-w-xl text-gray-700 focus:outline-none bg-gray-200 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-500 dark:hover:border-gray-800 hover:border-gray-400"/>
+                        </span>
+                    </div>
 
-                    <span class="flex mb-4 ml-12">Taak
-                    <input type='text' placeholder="Wat ik nodig heb is..." class="w-full mt-2 mb-6 px-6 py-3 ml-12 border rounded-lg text-lg max-w-xl text-gray-700 focus:outline-none bg-gray-200 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-500 dark:hover:border-gray-800 hover:border-gray-400"/>
-                    </span>
+                    <div>
+                        <span class="flex mb-4 ml-12">Taak:
+                        <input type='text' placeholder="Wat ik nodig heb is..." class="w-full mt-2 mb-6 px-6 py-3 ml-12 border rounded-lg text-lg max-w-xl text-gray-700 focus:outline-none bg-gray-200 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-500 dark:hover:border-gray-800 hover:border-gray-400"/>
+                        </span>
+                    </div>
 
-                    <span class="flex mb-4 ml-12">Actie:
-                    <input type='text' placeholder="Ik wil dat..." class="w-full mt-2 mb-6 px-6 py-3 ml-12 border rounded-lg text-lg max-w-xl text-gray-700 focus:outline-none bg-gray-200 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-500 dark:hover:border-gray-800 hover:border-gray-400"/>
-                    </span>
+                    <div>
+                        <span class="flex mb-4 ml-12">Actie:
+                        <input type='text' placeholder="Ik wil dat..." class="w-full mt-2 mb-6 px-6 py-3 ml-12 border rounded-lg text-lg max-w-xl text-gray-700 focus:outline-none bg-gray-200 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-500 dark:hover:border-gray-800 hover:border-gray-400"/>
+                        </span>
+                    </div>
 
-                    <span class="flex mb-4 ml-12">Resultaat:
-                    <input type='text' placeholder="Ik wil dat..." class="w-full mt-2 mb-6 px-6 py-3 ml-12 border rounded-lg text-lg max-w-xl text-gray-700 focus:outline-none bg-gray-200 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-500 dark:hover:border-gray-800 hover:border-gray-400"/>
-                    </span>
-                </span>
+                    <div>
+                        <span class="flex mb-4 ml-12">Resultaat:
+                        <input type='text' placeholder="Ik wil dat..." class="w-full mt-2 mb-6 px-6 py-3 ml-12 border rounded-lg text-lg max-w-xl text-gray-700 focus:outline-none bg-gray-200 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-500 dark:hover:border-gray-800 hover:border-gray-400"/>
+                        </span>
+                    </div>
             </div>
         </div>
         <div class="flex justify-center">
